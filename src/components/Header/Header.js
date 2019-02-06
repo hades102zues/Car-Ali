27167989e7d6f6fd20ae5991ab724f0fb21b65b6
Joshua Wiggins <joshua_kar@hotmail.com>
2019-02-06 16:41:14 -0400

    logo is now clickable

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,12 +2,18 @@ import React from "react";
 import styles from "./Header.module.css";
 import Container from "../../UI/Container/Container";
 import Navigation from "../Navigation/Navigation";
+import { withRouter } from "react-router-dom";
 
-const header = () => {
+const header = props => {
 	return (
 		<div className={styles.Header}>
 			<Container>
-				<h2 className={styles.logo}>CAR-ALI</h2>
+				<h2
+					className={styles.logo}
+					onClick={() => props.history.push("/")}
+				>
+					CAR-ALI
+				</h2>
 				<div className={styles.positionNav}>
 					<Navigation />
 				</div>
@@ -16,4 +22,4 @@ const header = () => {
 	);
 };
 
-export default header;
+export default withRouter(header);
