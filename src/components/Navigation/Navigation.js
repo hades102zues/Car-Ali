7f804b5fcbc113c76f426811684a2a5c6edb8465
Joshua Wiggins <joshua_kar@hotmail.com>
2019-02-06 16:36:21 -0400

    dynamically create navigation links

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,18 +3,35 @@ import NavigationItem from "./NavigationItem/NavigationItem";
 import styles from "./Navigation.module.css";
 
 const navigation = () => {
-	const navConfig = [
+	const isloggedin = false;
+
+	const baseNavConfigs = [
 		{ to: "/results", navName: "CATALOG" },
 		{ to: "/login", navName: "LOGIN" }
 	];
 
-	let navigationItems = navConfig.map(config => (
-		<NavigationItem
-			key={config.navName}
-			to={config.to}
-			navName={config.navName}
-		/>
-	));
+	const loggedInConfigs = [
+		{ to: "/results", navName: "CATALOG" },
+		{ to: "/user/dashboard", navName: "PROFILE" },
+		{ to: "/logout", navName: "LOGOUT" }
+	];
+
+	const navigationItems = isloggedin
+		? loggedInConfigs.map(config => (
+				<NavigationItem
+					key={config.navName}
+					to={config.to}
+					navName={config.navName}
+				/>
+		  ))
+		: baseNavConfigs.map(config => (
+				<NavigationItem
+					key={config.navName}
+					to={config.to}
+					navName={config.navName}
+				/>
+		  ));
+
 	return (
 		<nav>
 			<ul style={{ listStyle: "none", display: "flex" }}>
