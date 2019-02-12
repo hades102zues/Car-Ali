1ea5306d827b0bce434f67f8ce5a8aa84658914a
Joshua Wiggins <joshua_kar@hotmail.com>
2019-02-12 17:32:24 -0400

    cards layout completed

--- src/pages/ResultsPage/ResultsLister/Result/Result.js
+++ src/pages/ResultsPage/ResultsLister/Result/Result.js
@@ -3,7 +3,36 @@ import styles from "./Result.module.css";
 const result = props => {
 	return (
 		<div className={styles.result}>
-			<p>I am result</p>
+			<div className={styles.headerStatus} />
+			<div className={styles.imageBox}>
+				<img className={styles.img} src="" alt="car" />
+			</div>
+			<p className={styles.nameText}>CAR_NAME</p>
+
+			<div className={styles.infoBox}>
+				<div className={styles.info}>
+					<p className={styles.infoHeader}>YEAR</p>
+					<p className={styles.infoText}>{props.year}</p>
+				</div>
+				<div className={styles.info}>
+					<p className={styles.infoHeader}>PASSENGERS</p>
+					<p className={styles.infoText}>{props.passengers}</p>
+				</div>
+				<div className={styles.info}>
+					<p className={styles.infoHeader}>CONDITION</p>
+					<p className={styles.infoText}>{props.condition}</p>
+				</div>
+				<div className={styles.info}>
+					<p className={styles.infoHeader}>VERIFIED</p>
+					<p className={styles.infoText}>
+						{props.verified ? "YES" : "NO"}
+					</p>
+				</div>
+			</div>
+			<div className={styles.bidHighlight}>
+				<p className={styles.bidText}>HIGHEST BID</p>
+				<p className={styles.bidPrice}>$X.XX</p>
+			</div>
 		</div>
 	);
 };
--- src/pages/ResultsPage/ResultsLister/ResultsLister.js
+++ src/pages/ResultsPage/ResultsLister/ResultsLister.js
@@ -5,16 +5,59 @@ import Result from "./Result/Result";
 class ResultsLister extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {};
+		this.state = {
+			carListing: [
+				{
+					imageUrl:
+						"https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+					status: 0,
+					year: "2005",
+					condition: "3.5/5",
+					verified: true,
+					cost: "$500"
+				},
+				{
+					imageUrl:
+						"https://parkers-images.bauersecure.com/pagefiles/190699/cut-out/600x400/suzuki_swift10.jpg",
+					status: 1,
+					year: "",
+					condition: "",
+					verified: true
+				},
+				{
+					imageUrl:
+						"https://icdn2.digitaltrends.com/image/2018-subaru-crosstrek-front-left-640x640.jpg",
+					status: 1,
+					year: "",
+					condition: "",
+					verified: true
+				},
+				{
+					imageUrl:
+						"https://upload.wikimedia.org/wikipedia/commons/6/66/2015_Toyota_Fortuner_%28New_Zealand%29.jpg",
+					status: 1,
+					year: "",
+					condition: "",
+					verified: true
+				}
+			]
+		};
 	}
 
 	render() {
-		return (
-			<div className={styles.ResultsLister}>
-				<p>I am ResultsLister</p>
-				<Result />
-			</div>
-		);
+		const resultList = this.state.carListing.map((listing, i) => (
+			<Result
+				key={i}
+				cardWidth={this.props.cardWidth}
+				imageUrl={listing.imageUrl}
+				status={listing.status}
+				cost={listing.cost}
+				year={listing.year}
+				condition={listing.condition}
+				verified={listing.verified}
+			/>
+		));
+		return <div className={styles.ResultsLister}>{resultList}</div>;
 	}
 }
 
