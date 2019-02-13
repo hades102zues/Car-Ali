ec7ee0a27e71ee39b768155e4ff6c019631dd1b1
Joshua Wiggins <joshua_kar@hotmail.com>
2019-02-13 18:12:15 -0400

    bidview done

--- src/pages/BidDisplayPage/BidStrip/BidStrip.js
+++ src/pages/BidDisplayPage/BidStrip/BidStrip.js
@@ -4,13 +4,43 @@ import styles from "./BidStrip.module.css";
 class BidStrip extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {};
+		this.state = {
+			bidValue: 0
+		};
 	}
+
+	onInputChangeHandler = event => {
+		this.setState({ bidValue: event.target.value });
+	};
+
+	onBidSubmitHandler = () => {
+		//make call to server
+	};
 
 	render() {
 		return (
 			<div className={styles.BidStrip}>
-				<p>I am BidStrip</p>
+				<div className={styles.bidValue}>
+					<p className={styles.bidText}>
+						{this.state.bidValue
+							? `$${parseFloat(this.state.bidValue).toFixed(2)}`
+							: "$0"}
+					</p>
+				</div>
+				<form onSubmit={this.onBidSubmitHandler}>
+					<div className={styles.bidBox}>
+						<input
+							type="number"
+							placeholder="Enter your bid"
+							className={styles.bidInput}
+							value={this.state.bidValue}
+							onChange={this.onInputChangeHandler}
+						/>
+					</div>
+					<button type="submit" className={styles.bidButton}>
+						PLACE YOUR BID
+					</button>
+				</form>
 			</div>
 		);
 	}
