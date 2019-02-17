0136de75bb8b2eaedd82575ac068aef78a503e25
Joshua Wiggins <joshua_kar@hotmail.com>
2019-02-16 21:11:17 -0400

    now accepts users

--- Car-Ali_Server/controllers/login.js
+++ Car-Ali_Server/controllers/login.js
@@ -1,3 +1,39 @@
-exports.postSignup = (req, res) => {};
+const hasher = require("bcryptjs");
+const User = require("../models/users");
+const jwt = require("jsonwebtoken");
+const secret = process.env.SECRET || "Kil3rQue3nbiT5D4Dust";
+
+exports.postSignup = (req, res) => {
+	//check to see if the user exists
+	User.getOne(req, res, result => {
+		//if the result is not defined then user does not already exist
+		if (!result) {
+			//hash it
+			hasher
+				.hash(req.body.password, 10)
+				.then(hash => {
+					req.hash = hash;
+					User.create(req, res, result => {
+						//clear crucial data
+						delete req.hash;
+						delete req.body.password;
+
+						//create the token
+						const token = jwt.sign(req.body, secret);
+						//send back a token
+						res.status(200).json({
+							message: "New User Created!",
+							token: token
+						});
+					});
+				})
+				.catch(err => console.log("error while hashing"));
+		} else {
+			return res
+				.status(400)
+				.json({ message: "User With That Email Already Exists" });
+		}
+	});
+};
 
 exports.postLogin = (req, res) => {};
--- Car-Ali_Server/models/users.js
+++ Car-Ali_Server/models/users.js
@@ -1,13 +1,31 @@
-const knex = require("./knexfile").knex;
+const knex = require("../knexfile").knex;
 
 module.exports = class User {
 	//create
-	static create = (data, callback) => {
-		knex("users").insert();
-	};
+	static create(req, res, cb) {
+		knex("users")
+			.insert({
+				username: req.body.username,
+				name: req.body.name,
+				email: req.body.email,
+				password: req.hash,
+				profile_img: req.body.profile_img
+			})
+			.then(result => cb(result))
+			.catch(err =>
+				res.status(400).json({ message: "error creating user" })
+			);
+	}
 
 	//getOne
-	static getOne = (data, callback) => {};
+	static getOne(req, res, cb) {
+		knex("users")
+			.select()
+			.where({ email: req.body.email })
+			.first()
+			.then(result => cb(result))
+			.catch(err => console.log("Error getting user"));
+	}
 	//update
 
 	//delete
