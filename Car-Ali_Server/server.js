d320a754abcb6c4e930481fc8cb5b44350162a39
Joshua Wiggins <joshua_kar@hotmail.com>
2019-02-20 10:51:10 -0400

    multer setup completed

--- Car-Ali_Server/server.js
+++ Car-Ali_Server/server.js
@@ -3,6 +3,7 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3001;
 const bodyParser = require("body-parser");
+const multer = require("multer");
 
 const loginRoutes = require("./routes/login");
 const guardedlistingRoutes = require("./routes/guardedListing");
@@ -11,6 +12,33 @@ const authWare = require("./utility/authWare");
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
+
+//This portion prepares the files to be stored at a specific directory.
+//It determines what names the uploaded files will be stored under
+//and keeps the extension defined on the upload
+const fileStorage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		//this is how we specify where to store the files
+		//create these directories before hand!!!
+		cb(null, path.join(__dirname, "public", "uploads"));
+	},
+
+	filename: (req, file, cb) => {
+		//this specifies what names our uploaded files will be stored under
+		//.original name holds the name+extension of the uploaded file
+		cb(null, Date.now() + "_" + file.originalname);
+	}
+});
+
+const filterFunction = (req, file, cb) => {
+	file.mimetype === ".jpg" ||
+	file.mimetype === ".jpeg" ||
+	file.mimetype === "png"
+		? cb(null, true)
+		: cb(null, false);
+};
+
+const upload = multer({ storage: fileStorage, fileFilter: filterFunction });
 
 app.use(loginRoutes);
 app.use(unguardedlistingRoutes);
