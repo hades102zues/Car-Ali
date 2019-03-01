a409c077d6d9e1ef81527103dc4f8db64cc769a7
Joshua Wiggins <joshua_kar@hotmail.com>
2019-03-01 19:10:52 -0400

    Implemented the view functionality for the bid item. Awaiting creation of profile board html and css

--- src/pages/DashBoardPage/DashBoard/BidsBoard/BidLister/BidItem/BidItem.js
+++ src/pages/DashBoardPage/DashBoard/BidsBoard/BidLister/BidItem/BidItem.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import styles from "./BidItem.module.css";
+import { withRouter } from "react-router-dom";
 
 class BidItem extends Component {
 	constructor(props) {
@@ -24,7 +25,14 @@ class BidItem extends Component {
 					<p className={styles.cost}>{`$${this.props.cost}`}</p>
 				</div>
 				<div className={styles.right}>
-					<div className={styles.viewButton}>
+					<div
+						className={styles.viewButton}
+						onClick={() =>
+							this.props.history.push(
+								`/bid-view/${this.props.listingId}`
+							)
+						}
+					>
 						<img
 							src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAABfAAAAXwBsrqMZwAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAFOSURBVEiJ7ZS7SgNBFIb/DQQ0Jhh7LwF9glgn2Nv6CHkACxubiI15CCPYewMhT5DeoFUKBbdJYSemsshnkTOwxLObRFCbHDgszH+by85Ii/rvirJAoCzpQNK+pKqkkkEfkh4kdSRdRVH0PlcqkAdOgCHTawg0gfys5ltAbwbjyeoBm9PMN4AXR3wN1IA16zpw4/CegfU08wLw5IgOMyZ05PAfgYJHbnszT5xJC3i1bmF7Dtw5uvNJ8zowcog1w1sOdmbYnoONgjYEdB0SwIrhAwcbGFZK0XYlKWcZOWVX5n3J0gTjY0k4pKp9Lx0sjO06GOaZGIELZ5n3hoVDfrNOHnLH0bW/R0IR6DvkZtoeAKcOvw8U0wQ7QOyIwkVbApYZ/3W3Di8GttMmFEIq/PypqGSaJ0J+77GbCCoDDca3NQY+rWMbawCrcxsv6k/rCxV6IRC0rOvzAAAAAElFTkSuQmCC"
 							className={styles.img}
@@ -47,4 +55,4 @@ class BidItem extends Component {
 	}
 }
 
-export default BidItem;
+export default withRouter(BidItem);
